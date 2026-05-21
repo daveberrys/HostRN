466eae84429fa1815ddd00f2c949a8a8d3dd7cf3
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-05-22 02:42:39 +0800

    fix: forgotten `stop_service`

--- frontend/src/components/Service.js
+++ frontend/src/components/Service.js
@@ -1,6 +1,6 @@
 import html from "solid-js/html";
 import { For, createResource } from "solid-js";
-import { fetch_services, start_service, delete_service } from "../API.js";
+import { fetch_services, start_service, delete_service, stop_service } from "../API.js";
 
 function Service() {
     const [services] = createResource(fetch_services)
