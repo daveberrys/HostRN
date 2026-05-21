36924d0849704fe4e53ae6556b5c3cbd9a2cde70
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-05-22 03:06:09 +0800

    fix: use store instead

--- frontend/src/components/CreateService.js
+++ frontend/src/components/CreateService.js
@@ -1,21 +1,25 @@
 import html from 'solid-js/html';
 import { save_service } from '../API.js';
-import { signal } from 'solid-js';
+import { createStore } from 'solid-js/store';
 
 function CreateService() {
-    const services = signal({ name: '', path: '', command: '' });
+    const [form, setForm] = createStore({ 
+        name: '', 
+        path: '', 
+        command: '' 
+    });
     return html`
         <div class="CreateService">
             <section class="inputs">
-                <input type="text" oninput=${(e) => services.name = e.target.value} placeholder="Service Name" />
-                <input type="text" oninput=${(e) => services.path = e.target.value} placeholder="Service Path" />
-                <input type="text" oninput=${(e) => services.command = e.target.value} placeholder="Service Command" />
+                <input type="text" oninput=${(e) => setForm({ name: e.target.value })} placeholder="Service Name" />
+                <input type="text" oninput=${(e) => setForm({ path: e.target.value })} placeholder="Service Path" />
+                <input type="text" oninput=${(e) => setForm({ command: e.target.value })} placeholder="Service Command" />
             </section>
 
             <section class="buttons">
                 <button
                     class="true"
-                    onclick=${() => save_service(services.name, services.path, services.command)}
+                    onclick=${() => save_service(form.name, form.path, form.command)}
                 >
                     Save Service
                 </button>
